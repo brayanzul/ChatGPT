68799e532ccf25110288ae1279942f365d92c6f7
Brayan Zuluaga <brayanzuluaga93@hotmail.com>
2023-02-28 20:29:47 -0500

    Add The ChatGPT Project and work: Api Consts, Constants, Chat Screen, Api Servcice, Services, Drop Down, Chat Models, Models Model, Chats Provider, Models Provider, Chat Widget

---
+++ lib/providers/chats_provider.dart
@@ -0,0 +1,26 @@
+import 'package:chatgpt/services/api_servcice.dart';
+import 'package:flutter/material.dart';
+
+import '../models/chat_models.dart';
+
+class ChatProvider with ChangeNotifier {
+  List<ChatModel> chatList = [];
+  List<ChatModel> get getChatList {
+    return chatList;
+  }
+
+  void addUserMessage({required String msg}) {
+    chatList.add(ChatModel(msg: msg, chatIndex: 0));
+    notifyListeners();
+  }
+
+  Future<void> sendMessageAndGetAnswers(
+    {required String msg, required String chosenModelId}) async {
+      chatList.addAll(await ApiService.sendMessage(
+        message: msg,
+         modelId: chosenModelId,
+      ));
+      notifyListeners();
+    }
+
+}
